510e7b4ee6c049bd8d4f90c0351870447339a3dd
bayloremily <emily_schaedle@baylor.edu>
2026-06-09 15:40:03 -0500

    Fix Vercel base path deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => ({
-  base:
-    command === 'serve'
-      ? '/'
-      : mode === 'scorm'
-        ? './'
-        : '/mft5305-diagnostic-clinic-simulator/',
+  base: command === 'serve' ? '/' : mode === 'scorm' ? './' : '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500,
